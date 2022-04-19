30b45feae4fcb64260bb9dfec9548df89b1f1bea
小林優一 <y.k94cofitec179@gmail.com>
2022-04-19 18:00:15 +0900

    useContextを用いてデータを取得

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import "./App.css";
+import React, { createContext, useState } from "react";
 import MouseEventEffect from "./components/MouseEventEffect";
 // import EffectHook from "./components/EffectHook";
 // import DataFetch from "./components/DataFetch";
 import DataFetchById from "./components/DataFetchById";
+import ComponentC from "./components/ComponentC";
 
+export const UserContext = createContext();
+export const LanguageContext = createContext();
 function App() {
+  const [user, setUser] = useState({ name: "yamda", age: "32" });
+  const [language, setLanguage] = useState("日本語");
   return (
     <div className="App">
-      {/* <DataFetch /> */}
-      <DataFetchById />
+      <UserContext.Provider value={user}>
+        <LanguageContext.Provider value={language}>
+          <ComponentC />
+        </LanguageContext.Provider>
+      </UserContext.Provider>
     </div>
   );
 }
---
+++ src/components/ComponentF.js
@@ -0,0 +1,28 @@
+import React, { useContext } from "react";
+import { UserContext, LanguageContext } from "../App";
+function ComponentF() {
+  const user = useContext(UserContext);
+  const language = useContext(LanguageContext);
+  return (
+    <div>
+      {user.name}:{language}
+      {/*   <UserContext.Consumer>
+        {(user) => {
+          return (
+            <LanguageContext.Consumer>
+              {(language) => {
+                return (
+                  <div>
+                    {user.name}:{language}
+                  </div>
+                );
+              }}
+            </LanguageContext.Consumer>
+          );
+        }}
+      </UserContext.Consumer> */}
+    </div>
+  );
+}
+
+export default ComponentF;
