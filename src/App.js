0c8c4d0b7f755540e788787861c3176070c7d870
小林優一 <y.k94cofitec179@gmail.com>
2022-05-04 18:16:24 +0900

    useRefを使用してカウントをストップした。

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
-import WrapComponent from "./components/WrapComponent";
+import Check from "./components/Check";
+import Count from "./components/Count";
+import FocusInput from "./components/FocusInput";
 
 function App() {
   return (
     <div className="App">
-      <WrapComponent />
+      <h1>useRef</h1>
+      <Count />
     </div>
   );
 }
--- src/components/Count.js
+++ src/components/Count.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
-function Count({ text, count }) {
-  console.log("Count component -", text);
+function Count() {
+  const [count, setCount] = useState(0);
+  const intervalRef = useRef();
+  useEffect(() => {
+    intervalRef.current = setInterval(() => {
+      setCount((prevCount) => prevCount + 1);
+    }, 1000);
+    return () => {
+      clearInterval(intervalRef.current);
+    };
+  }, []);
   return (
     <div>
-      {text}:{count}
+      <h1>{count}</h1>
+      <button onClick={() => clearInterval(intervalRef.current)}>
+        ストップ
+      </button>
     </div>
   );
 }
 
-export default React.memo(Count);
+export default Count;
