a73d8c13e07a891a13d1e1a7fa94c0ccba0b4289
小林優一 <y.k94cofitec179@gmail.com>
2022-05-05 17:46:55 +0900

    customHookを作成した。

---
+++ src/components/hooks/useDocumentTitle.js
@@ -0,0 +1,9 @@
+import { useEffect } from "react";
+
+function useDocumentTitle(count) {
+  useEffect(() => {
+    document.title = `カウント ${count}`;
+  }, [count]);
+}
+
+export default useDocumentTitle;
